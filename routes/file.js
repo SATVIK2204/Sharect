9f4aba4ca08edc23c085c62895089142bfe3cc8e
SATVIK2204 <bksatvik@gmail.com>
2021-12-24 18:31:10 +0530

    Removed Repo Bug

--- routes/file.js
+++ routes/file.js
@@ -8,16 +8,12 @@ const { v4: uuidv4 } = require('uuid');
 let storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, 'uploads/'),
   filename: (req, file, cb) => {
-    const uniqueName = `${Date.now()}-${Math.round(
-      Math.random() * 1e9
-    )}${path.extname(file.originalname)}`;
+    const uniqueName = `${Date.now()}-${Math.round(Math.random() * 1e9)}${path.extname(file.originalname)}`;
     cb(null, uniqueName);
   },
 });
 
-let upload = multer({ storage, limits: { fileSize: 1000000 * 100 } }).single(
-  'myfile'
-); //100mb
+let upload = multer({ storage, limits: { fileSize: 1000000 * 100 } }).single('myfile'); //100mb
 
 router
   .route('/')
@@ -34,7 +30,7 @@ router
       });
       const response = await file.save();
       res.json({ file: `${process.env.APP_BASE_URL}/files/${response.uuid}` });
-      console.log(file+" ");
+      console.log(file + ' ');
     });
   })
   .get((req, res) => {
